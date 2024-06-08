d2ebe2c26601edefc115f936310caa61e7ff7085
Eunryong Lee <eunrlee@c2r8s3.42seoul.kr>
2024-06-08 17:27:19 +0900

    fix: compare

--- src/ft_ls/utils.c
+++ src/ft_ls/utils.c
@@ -12,19 +12,14 @@ void* ft_realloc(void* src, size_t cur_size, size_t size, size_t elem_size) {
     return ret;
 }
 
-bool check_root(char *path) {
-	if (ft_strncmp(path, "./", 3) == 0) return true;
-	return false;
-}
-
 long compare(t_file a, t_file b, t_flag flag) {
 	long ret;
 	if (flag.time == false) {
-		ret = ft_strncmp(check_root(a.name) == true ? "" : a.name, check_root(b.name) == true ? "" : b.name, max(a.name_len, b.name_len));
+		ret = ft_strncmp(a.name, b.name, max(a.name_len, b.name_len));
 	} else {
 		ret = b.stat.st_mtimespec.tv_sec - a.stat.st_mtimespec.tv_sec;
 		if (ret == 0) {
-			ret = ft_strncmp(check_root(a.name) == true ? "" : a.name, check_root(b.name) == true ? "" : b.name, max(a.name_len, b.name_len));
+			ret = ft_strncmp(a.name, b.name, max(a.name_len, b.name_len));
 		}
 	}
 	if (flag.reverse) ret *= -1;
@@ -53,7 +48,7 @@ void q_sort(t_file *file_list, int start, int end, t_flag flag) {
 }
 
 int compare_str(char *a, char *b, t_flag flag) {
-	int ret = ft_strncmp(check_root(a) == true ? "" : a, check_root(b) == true ? "" : b, max(ft_strlen(a), ft_strlen(b)));
+	int ret = ft_strncmp(a, b, max(ft_strlen(a), ft_strlen(b)));
 	if (flag.reverse) ret *= -1;
 	return ret;
 }
